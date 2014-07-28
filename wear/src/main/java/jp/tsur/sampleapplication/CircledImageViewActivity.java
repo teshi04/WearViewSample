cfd81036e8049da7e59c21f599a9acdfa178b3c8
teshi <teshi04@gmail.com>
2014-07-28 16:01:45 +0900

    setOnClickListener する

--- wear/src/main/java/jp/tsur/sampleapplication/CircledImageViewActivity.java
+++ wear/src/main/java/jp/tsur/sampleapplication/CircledImageViewActivity.java
@@ -2,9 +2,8 @@ package jp.tsur.sampleapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import jp.tsur.sampleapplication.R;
+import android.view.View;
+
 
 public class CircledImageViewActivity extends Activity {
 
@@ -12,6 +11,12 @@ public class CircledImageViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circled_image_view);
+        findViewById(R.id.circle_image_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
